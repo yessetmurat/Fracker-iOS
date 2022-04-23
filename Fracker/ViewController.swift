0ca0ed9d3fbe883cf7c9005cc776aefa1867d7b4
Yesset Murat <yesset.murat@icloud.com>
2022-04-23 21:15:16 +0600

    Initial Commit

---
+++ Fracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fracker
+//
+//  Created by Yesset Murat on 4/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
