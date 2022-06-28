3b1d972f8820858bbf6401a892855e7b53f8c239
Yesset Murat <yesset.murat@icloud.com>
2022-06-28 18:11:31 +0600

    Added development key

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -5,6 +5,7 @@ set -eo pipefail
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/profile.mobileprovision ./.github/secrets/profile.mobileprovision.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/development.mobileprovision ./.github/secrets/development.mobileprovision.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/ios_distribution.p12 ./.github/secrets/ios_distribution.p12.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/development.p12 ./.github/secrets/development.p12.gpg
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
@@ -13,6 +14,7 @@ cp ./.github/secrets/development.mobileprovision ~/Library/MobileDevice/Provisio
 
 security create-keychain -p "" build.keychain
 security import ./.github/secrets/ios_distribution.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_KEYS" -A
+security import ./.github/secrets/development.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_KEYS" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
