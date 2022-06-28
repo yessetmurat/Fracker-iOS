446fdc77d4e303cb3b562ee1dd27296b96d41741
Yesset Murat <yesset.murat@icloud.com>
2022-06-28 20:31:02 +0600

    Removed files

--- .github/scripts/archive.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-xcodebuild -scheme Fracker \
-            -sdk iphoneos \
-            -configuration Release \
-            -archivePath $PWD/build/Fracker.xcarchive \
-            clean archive | xcpretty
